006e32f93feb1582e86e4087d469288113940fdc
apwic <adiyansawicaksana@gmail.com>
2024-05-18 09:43:53 +0000

    feat: ia3 and unipelt script for sentiment

---
+++ sentiment/run_sentiment_unipelt.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+export WANDB_PROJECT="indolem-pelt"
+MAX_LENGTH=200
+BERT_MODEL="indolem/indobert-base-uncased"
+BATCH_SIZE=30
+NUM_EPOCHS=20
+LEARNING_RATE=5e-5
+SEED=1
+
+DATA_DIR=./data
+
+for i in {0..0}
+do
+    echo "Training on fold $i with UniPELT"
+    
+    OUTPUT_DIR="bin/sentiment-unipelt"
+    TRAIN_FILE="$DATA_DIR/train$i.csv"
+    VALIDATION_FILE="$DATA_DIR/dev$i.csv"
+    TEST_FILE="$DATA_DIR/test$i.csv"
+    
+    # Run the model training and evaluation
+    python run_sentiment.py \
+        --model_name_or_path $BERT_MODEL \
+        --label_names "labels" \
+        --output_dir $OUTPUT_DIR \
+        --train_file $TRAIN_FILE \
+        --validation_file $VALIDATION_FILE \
+        --test_file $TEST_FILE \
+        --max_seq_length $MAX_LENGTH \
+        --num_train_epochs $NUM_EPOCHS \
+        --per_device_train_batch_size $BATCH_SIZE \
+        --learning_rate $LEARNING_RATE \
+        --evaluation_strategy "epoch" \
+        --logging_strategy "epoch" \
+        --save_strategy "epoch" \
+        --save_total_limit 1 \
+        --report_to "tensorboard" "wandb" \
+        --push_to_hub \
+        --run_name "sentiment-unipelt" \
+        --do_train \
+        --do_eval \
+        --do_predict \
+        --overwrite_output_dir \
+        --adapter_config "unipelt" \
+        --adapter_name "sentiment-unipelt" \
+        --train_adapter \
+
+    echo "Finished training on fold $i with UniPELT"
+done
