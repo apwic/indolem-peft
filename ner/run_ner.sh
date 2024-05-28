24a0ea26e22afff33bda6d0d9ae09628a0a4426d
apwic <adiyansawicaksana@gmail.com>
2024-05-28 14:16:51 +0000

    feat: adding run script to execute all

---
+++ ner/run_ner.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source ./script/run_ner_base.sh
+source ./script/run_ner_lora.sh
+source ./script/run_ner_pt.sh
+source ./script/run_ner_unipelt.sh
---
+++ sentiment/run_sentiment.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source ./script/run_sentiment_base.sh
+source ./script/run_sentiment_lora.sh
+source ./script/run_sentiment_pt.sh
+source ./script/run_sentiment_unipelt.sh
