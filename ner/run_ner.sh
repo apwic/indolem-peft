4a36f94e136d3f5b67c525efa8d4632d2b6b960f
apwic <adiyansawicaksana@gmail.com>
2024-06-03 23:37:32 +0700

    feat: change NER script to use both dataset in one .sh

--- ner/run_ner.sh
+++ ner/run_ner.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+export DATASET=nerugm
+source ./script/run_ner_base.sh
+source ./script/run_ner_lora.sh
+source ./script/run_ner_pt.sh
+source ./script/run_ner_unipelt.sh
+
+export DATASET=nerui
 source ./script/run_ner_base.sh
 source ./script/run_ner_lora.sh
 source ./script/run_ner_pt.sh
