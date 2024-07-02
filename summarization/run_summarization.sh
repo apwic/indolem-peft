7b665f7ec397598770635f73f7212d2c1257bb40
apwic <adiyansawicaksana@gmail.com>
2024-07-02 13:31:39 +0700

    feat: adapters script for summarization

---
+++ summarization/run_summarization.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source ./script/run_summarization_base.sh
+source ./script/run_summarization_lora.sh
+source ./script/run_summarization_pt.sh
+source ./script/run_summarization_seq_bn.sh
+source ./script/run_summarization_unipelt.sh
