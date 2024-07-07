848bc6c1d1f410ffa6052db54d646a6c50d858d0
apwic <adiyansawicaksana@gmail.com>
2024-07-08 02:34:46 +0700

    adding labels.txt for nerui

--- ner/get_label.sh
+++ ner/get_label.sh
@@ -1,10 +1,4 @@
-export DATA_DIR=./data/nerugm
+export DATASET=nerugm
+export DATA_DIR=./data/$DATASET
 
-for i in {0..4}
-do
-    cat $DATA_DIR/train$i.tsv | tr '\t' ' '  | tr '  ' ' ' > $DATA_DIR/train.txt
-    cat $DATA_DIR/dev$i.tsv  | tr '\t' ' '  | tr '  ' ' ' > $DATA_DIR/dev.txt
-    cat $DATA_DIR/test$i.tsv  | tr '\t' ' '  | tr '  ' ' ' > $DATA_DIR/test.txt
-done
-
-cat $DATA_DIR/train.txt $DATA_DIR/dev.txt $DATA_DIR/test.txt | cut -d " " -f 2 | grep -v "^$"| sort | uniq > $DATA_DIR/labels.txt
+cat $DATA_DIR/$DATASET.txt | cut -d " " -f 2 | grep -v "^$"| sort | uniq > $DATA_DIR/labels.txt
