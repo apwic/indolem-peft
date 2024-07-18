0ff8f7135d3d973bc72974fb75f47bd5acfb76c1
apwic <adiyansawicaksana@gmail.com>
2024-07-18 23:03:31 +0700

    chore: moving indosum script

--- summarization/run_summarization.sh
+++ summarization/run_summarization.sh
@@ -5,7 +5,7 @@ export EVAL_BATCH_SIZE=8
 export NUM_EPOCHS=5
 export LEARNING_RATE=1e-3
 export MAX_LENGTH=512
-export GEN_MAX_LENGTH=128
+export GEN_MAX_LENGTH=512
 export NUM_BEAMS=5
 export WEIGHT_DECAY=0.01
 export PATIENCE=5
@@ -13,8 +13,8 @@ export SEED=42
 export DATASET=indosum
 export DATA_DIR=./data/$DATASET
 
-source ./script/run_summarization_base.sh
-source ./script/run_summarization_lora.sh
-source ./script/run_summarization_pt.sh
-source ./script/run_summarization_seq_bn.sh
-source ./script/run_summarization_unipelt.sh
+source ./script/$DATASET/run_summarization_base.sh
+source ./script/$DATASET/run_summarization_lora.sh
+source ./script/$DATASET/run_summarization_pt.sh
+source ./script/$DATASET/run_summarization_seq_bn.sh
+source ./script/$DATASET/run_summarization_unipelt.sh
